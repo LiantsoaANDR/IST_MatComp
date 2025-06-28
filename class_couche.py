baa50133c326085ce8059e2630713b83dcf657fd
LiantsoaANDR <liantsoa.cpro@gmail.com>
2025-06-28 18:54:27 +0300

    idk

--- class_couche.py
+++ class_couche.py
@@ -1,5 +1,15 @@
-class Couche():
+class Couche:
     """
     Classe servant à créer des couches
     """
-    pass
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Initialize the new object"""
+        if id is not None:
+            self.id = id
+        else:
+            Couche.__nb_objects += 1
+            self.id = Couche.__nb_objects
+
+    
